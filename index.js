a60f2853b4cc0b0d7cf1e3bb9f57115789bcfb51
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-07 22:54:06 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+});
+
+bot.on("message", async message => {
+  if(message.author.bot) return;
+  if(message.channel.type === "dm") return;
+  let messageArray = message.content.split(" ");
+  let cmd = messageArray[0];
+  let args = messageArray.slice(1);
+  
+  if (message.content.includes('test')) {
+   message.channel.send("TEST!")
+  }
+});
+
+bot.login(process.env.BOT_TOKEN);
