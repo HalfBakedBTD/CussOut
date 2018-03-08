750adac0e3164006d19d5b94c85f8c1fa4ffa829
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-08 17:50:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,62 +1,38 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const bot = new Discord.Client({disableEveryone: true});
+const badWords = [
+    'nigga',
+    'nigger',
+    'fuck',
+    'shit',
+    'asshole',
+    'cunt',
+    'fag',
+    'fuk',
+    'fck',
+    'fcuk',
+    'assfuck',
+    'assfucker',
+    'fucker',
+    'dick'
+];
 
 bot.on("ready", async () => {
-  console.log(`${bot.user.username} is online!`);
+    console.log(`${bot.user.username} is online!`);
 });
 
 bot.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.channel.type === "dm") return;
-  let messageArray = message.content.split(" ");
-  let cmd = messageArray[0];
-  let args = messageArray.slice(1);
-  
-  if (message.content.toLowerCase().includes('fuck')) {
-    message.delete().catch(O_o=>{});
-    message.channel.send(`**__VULGAR DELETED__**`).then(msg => msg.delete(2000));
-  }
-  if (message.content.toLowerCase().includes('dick')) {
-    message.delete().catch(O_o=>{});
-    message.channel.send(`**__VULGAR DELETED__**`).then(msg => msg.delete(2000));
-  }
-  if (message.content.toLowerCase().includes('cunt')) {
-    message.delete().catch(O_o=>{});
-    message.channel.send(`**__VULGAR DELETED__**`).then(msg => msg.delete(2000));
-  }
-  if (message.content.toLowerCase().includes('nigger')) {
-    message.delete().catch(O_o=>{});
-    message.channel.send(`**__VULGAR DELETED__**`).then(msg => msg.delete(2000));
-  }
-  if (message.content.toLowerCase().includes('nigga')) {
-    message.delete().catch(O_o=>{});
-    message.channel.send(`**__VULGAR DELETED__**`).then(msg => msg.delete(2000));
-  }
-  if (message.content.toLowerCase().includes('fu')) {
-    message.delete().catch(O_o=>{});
-    message.channel.send(`**__VULGAR DELETED__**`).then(msg => msg.delete(2000));
-  }
-  if (message.content.toLowerCase().includes('crap')) {
-    message.delete().catch(O_o=>{});
-    message.channel.send(`**__VULGAR DELETED__**`).then(msg => msg.delete(2000));
-  }
-  if (message.content.toLowerCase().includes('bitch')) {
-    message.delete().catch(O_o=>{});
-    message.channel.send(`**__VULGAR DELETED__**`).then(msg => msg.delete(2000));
-  }
-  if (message.content.toLowerCase().includes('wtf')) {
-    message.delete().catch(O_o=>{});
-    message.channel.send(`**__VULGAR DELETED__**`).then(msg => msg.delete(2000));
-  }
-  if (message.content.toLowerCase().includes('lmao')) {
-    message.delete().catch(O_o=>{});
-    message.channel.send(`**__VULGAR DELETED__**`).then(msg => msg.delete(2000));
-  }
-  if (message.content.toLowerCase().includes('lma')) {
-    message.delete().catch(O_o=>{});
-    message.channel.send(`**__VULGAR DELETED__**`).then(msg => msg.delete(2000));
-  }
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+    for (i = 0; i < badWords.length; i++) {
+        var rgx = new RegExp(badWords[i], 'gi');
+        if (rgx.test(message.content)) {
+            message.delete().catch(O_o=>{});
+            message.channel.send("**__VULGAR DELETED__**").then(msg => msg.delete(2000));
+            return;
+        }
+    }
 });
 
 bot.login(process.env.BOT_TOKEN);
