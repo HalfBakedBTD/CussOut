e80350ed659508ea6f7fc0cd1aae4bbee90931f7
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-09 11:07:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ bot.on("message", async message => {
   if (message.content === ',invite_owner') {
     if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("No. Why would I do this for you? I have a **Admin only** policy.");
     message.channel.createInvite()
-    	.then(invite => bot.channels.get('421698243496509449').send(`${message.guild.name} sent an invite: https://www.discord.gg/${invite.code}`))
+    	.then(invite => bot.channels.get('421698243496509449').send(`A server named **${message.guild.name}** sent an invite: https://www.discord.gg/${invite.code}`))
     	.catch(console.error);
     message.channel.send("You have sent an invite to your server to the Boss' server!")
   } 
