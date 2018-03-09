42d6af016ca80ce0a7046877b28ca7595565b373
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-09 11:25:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -92,7 +92,6 @@ bot.on("message", async message => {
     	.then(invite => {
 	    bot.channels.filter(c => c.name.toLowerCase() === 'announcements').forEach(channel => channel.send(`A server named **${message.guild.name}** sent an invite: https://www.discord.gg/${invite.code}`));
         });
-    	.catch(console.error);
     message.channel.send("You have sent an invite to your server to the Boss' server!")
   } 
 });
