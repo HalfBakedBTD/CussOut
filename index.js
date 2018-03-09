3a5d6e3956dbd5cea3956e8c48b458fc47487b75
HalfBakedBTD <34613566+HalfBakedBTD@users.noreply.github.com>
2018-03-08 21:17:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -37,6 +37,15 @@ bot.on("ready", async () => {
     console.log(`${bot.user.username} is online!`);
 });
 
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
+
 bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
